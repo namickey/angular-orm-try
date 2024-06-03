02dedd6757f5d0792a47434a8de10a3e0ee41293
namickey <teethdai@gmail.com>
2024-06-04 07:14:52 +0900

    add httpClient

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+  
+  mydata = '[]'
+  #http = inject(HttpClient)
+  constructor(private http: HttpClient) {
+    this.#http.get('http://localhost:3000/users/')
+    .subscribe(data => {
+      this.mydata = JSON.stringify(data);
+    });
+  }
 }
